f8961b14a0acb1d7ebcb2eb2b0de2b6ac12fbb0e
Jaroslav Podhorný <lpodhorny075@gmail.com>
2024-04-12 22:42:52 +0200

    přidání hlavních skrptů

---
+++ car_module.py
@@ -0,0 +1,50 @@
+import pygame
+
+class car_object:
+
+    def __init__(self, img, fps):
+        self.img = img
+        self.fps = fps
+        
+        self.carx = 100
+        self.cary = 100
+        self.rotation = 0
+        self.wheel_rotation = 0  # hodnota mezi 0 - neco jako 0.7 !ne jedna!
+
+        self.pix_per_sec = 100
+        self.speed = 0
+    
+    def get_input():
+        keys = pygame.key.get_pressed()
+        input = [0,0]
+        
+        if keys[pygame.K_LEFT]:
+            input[0] = -1
+        if keys[pygame.K_RIGHT]:
+            input[0] = 1
+        if keys[pygame.K_DOWN]:
+            input[1] = 1
+        if keys[pygame.K_UP]:
+            input[1] = -1
+        
+        return input
+
+
+    def show(self, screen):
+
+        screen.blit(pygame.transform.rotate(self.img, self.rotation), (self.carx + (self.img.get_width() / 2), self.cary + (self.img.get_height() / 2)))
+        
+    
+    def update(self):
+        h_input = car_object.get_input()[0]
+        v_input = car_object.get_input()[1]
+
+        self.speed = (self.pix_per_sec/self.fps) # udelat by jel dopredu pouze stranou kterou faceuje
+        self.rotation += -h_input
+        pass
+
+
+
+
+    
+        
---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import car_module
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+ 
+fps = 60
+fpsClock = pygame.time.Clock()
+
+pygame.display.set_caption("Auto, který jede")
+screen = pygame.display.set_mode((1020,820))
+
+car_img = pygame.transform.scale_by(pygame.image.load("car.png"), 0.25)
+mycar = car_module.car_object(car_img, fps)
+ 
+while True:
+    screen.fill((154, 218, 111))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # hra
+    mycar.update()
+    mycar.show(screen)
+
+    # hra
+              
+    pygame.display.flip()
+    fpsClock.tick(fps)
+
