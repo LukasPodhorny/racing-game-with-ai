ef70f789999b62247ee01d1270e9f6c3d734fb87
LukasPodhorny <112276946+LukasPodhorny@users.noreply.github.com>
2024-04-30 19:47:14 +0200

    line intersection

--- usefulfunctions.py
+++ usefulfunctions.py
@@ -1,32 +1,28 @@
 import math
 import pygame
 from settings import *
+from scipy.interpolate import interp1d
 
-def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
+def calculateOffsets (A, B, C, D):
+        
+    top = (D.y - C.y) * (A.x - C.x) - (D.x - C.x) * (A.y - C.y)
+    bottom = (D.x - C.x) * (B.y - A.y) - (D.y - C.y) * (B.x - A.x)
+    if bottom != 0.0:
+        offset = top / bottom
+        if (offset >= 0 and offset <= 1):
+            return offset
+    
+    return None
 
-    # Calculate slopes of the lines
-    m1 = (y2 - y1) / (x2 - x1) if x2 != x1 else None
-    m2 = (y4 - y3) / (4 - x3) if x4 != x3 else None
+def getIntersection(A, B, C, D):
+    t = calculateOffsets(A, B, C, D)
+    u = calculateOffsets(C, D, A, B)
     
-    # If both lines are vertical (parallel)
-    if m1 is None and m2 is None:
-        return None
-    
-    # If one of the lines is vertical
-    if m1 is None:
-        x = x1
-        y = m2 * (x - x3) + y3
-    elif m2 is None:
-        x = x3
-        y = m1 * (x - x1) + y1
-    else:
-        # If lines are not parallel, find the intersection point
-        if m1 == m2:
-            return None  # Lines are parallel, no intersection
-        x = ((m1 * x1 - m2 * x3) + y3 - y1) / (m1 - m2)
-        y = m1 * (x - x1) + y1
-    
-    return (x, y)
+    if t and u:
+        interp = interp1d(A, B)
+        return interp(t)
+                    
+    return None
 
 def distance(point1, point2):
     return math.sqrt(math.pow(point1[0] - point2[0],2) + math.pow(point1[1] - point2[1],2))
